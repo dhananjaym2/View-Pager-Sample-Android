9957a28813da8283f1988f95c8298e78ecbb9b11
Dhananjay <dhananjaymohnot@gmail.com>
2018-07-13 00:58:50 +0530

    Display image URL in textView on each fragment in view pager.

--- app/src/main/java/view/pager/sample/android/ScreenSlidePageFragment.java
+++ app/src/main/java/view/pager/sample/android/ScreenSlidePageFragment.java
@@ -7,18 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ScreenSlidePageFragment extends Fragment {
     private ArrayList<ImageData> imageDataArrayList;
     private int currentDisplayPosition;
+    private TextView urlTextView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
 
+        urlTextView = rootView.findViewById(R.id.urlTextView);
         receiveDataFromBundle();
         setDataToShow();
         return rootView;
@@ -32,5 +35,7 @@ public class ScreenSlidePageFragment extends Fragment {
         Bundle bundle = getArguments();
         currentDisplayPosition = bundle.getInt(AppConstants.BUNDLE_KEY_IMAGE_OPENED);
         imageDataArrayList = bundle.getParcelableArrayList(AppConstants.BUNDLE_KEY_IMAGE_DATA_ARRAY);
+        if (urlTextView != null)
+            urlTextView.setText(imageDataArrayList.get(currentDisplayPosition).getImageUrl());
     }
 }
